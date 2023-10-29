220af7ff15dee700914eae53d1db0f3799b5d4df
Daniel Ochoja <danielochoja@yahoo.com>
2023-10-29 16:05:13 +0100

    updated

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+"""Return sum of list"""
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """Return list of float"""
     total: float = 0
 
     for num in input_list:
