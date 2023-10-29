c25cfd72623e7d8e18173207686b6d8ad148a8b7
Daniel Ochoja <danielochoja@yahoo.com>
2023-10-29 14:14:22 +0100

    updated

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,6 +2,6 @@
 """Takes strings and returns concatenated strings"""
 
 
-def concat(str1:str, str2:str) -> str:
+def concat(str1: str, str2: str) -> str:
     """Returns concatenated string"""
     return str1 + str2
