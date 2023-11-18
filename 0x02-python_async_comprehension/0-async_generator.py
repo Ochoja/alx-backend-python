eb65b8dafd567330746c96fa93f330313c207def
Daniel Ochoja <danielochoja@yahoo.com>
2023-11-18 18:35:36 +0100

    updated

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""An async generator"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """Returns a generator"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,7 @@
+# 0x02 Python - Async Comprehension
+
+## Areas Covered
+
+<li>How to write an asynchronous generator</li>
+<li>How to use async comprehensions</li>
+<li>How to type-annotate generators</li>
