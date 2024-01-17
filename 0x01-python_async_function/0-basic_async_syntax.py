3fcb12b4b6acb8b37313a2a17a8e608992be5ee4
Daniel Ochoja <danielochoja@yahoo.com>
2024-01-17 08:05:51 +0100

    updated

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,8 +5,8 @@ import asyncio
 from typing import Union
 
 
-async def wait_random(max_delay: Union[int, float] = 10) -> float:
+async def wait_random(max_delay: Union[int, float] = 10) -> int:
     """wait a random time and return same random number"""
-    delay = random.uniform(0, max_delay)
+    delay = random.uniform(0, int(max_delay))
     await asyncio.sleep(delay)
     return delay
